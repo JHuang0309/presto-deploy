73ceff8eb404f95bc4eaeec991d6cc3353478d66
Jayden Huang <jaydenhuang@student.unsw.edu.au>
2024-11-01 14:58:05 +1100

    Added login component

---
+++ frontend/src/components/Logout.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+const Logout = ({ token, setTokenFn}) => {
+
+    const navigate = useNavigate();
+
+    const logout = () => {  
+        axios.post('http://localhost:5005/admin/auth/logout', {}, {
+            headers: { Authorization: `Bearer ${token}`}
+        })
+        .then(res => {
+            localStorage.removeItem('token');
+            setTokenFn(null);
+            navigate('/login');
+        })
+        .catch(res => {
+            console.log(res.response.data.error);
+        })
+    };
+
+    return (
+        <button onClick={logout}>Logout</button>
+    );
+    
+}
+
+
+
+export default Logout

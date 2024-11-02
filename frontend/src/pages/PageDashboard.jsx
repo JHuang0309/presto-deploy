dba839239cefe368457830863ad9ea38d84e0e37
Alyssa Xia <z5420334@ad.unsw.edu.au>
2024-11-02 16:50:49 +1100

    Presentation category styling

--- frontend/src/pages/PageDashboard.jsx
+++ frontend/src/pages/PageDashboard.jsx
@@ -1,6 +1,13 @@
 function PageDashboard() {
     return (
         <>
+            <div className="ml-4">
+                <h3 className="text-balance text-3xl font-semibold">Start a new presentation</h3>
+            </div>
+            <hr className="ml-4"/>
+            <div className="ml-4">
+                <h3 className="text-balance text-3xl font-semibold">My presentations</h3>
+            </div>
         </>
     )
 }

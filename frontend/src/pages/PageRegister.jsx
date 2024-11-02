c270b4811708e8fc5102189e59176b5ed1f12bb1
Jayden Huang <jaydenhuang@student.unsw.edu.au>
2024-11-02 12:20:26 +1100

    Register page styling complete

--- frontend/src/pages/PageRegister.jsx
+++ frontend/src/pages/PageRegister.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate, Link } from 'react-router-dom'; 
 
 function PageRegister({ setTokenFn }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confPassword, setConfPassword] = useState('');
     const [name, setName] = useState('');
 
     const navigate = useNavigate();
@@ -29,42 +30,79 @@ function PageRegister({ setTokenFn }) {
 
     return (
         <>
-            <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Register</h2>
-            <form>
+            <div className="mx-auto max-w-2xl text-center mt-10">
+                <h2 className="text-balance text-4xl font-semibold sm:text-5xl">Welcome to Presto!</h2>
+                <p className='mt-2 text-lg/8 text-gray-600'>Let's get you signed up</p>
+            </div> 
+            <form className="mx-auto mt-10 max-w-xl sm:mt-10 px-10">
                 <div>
-                    Email: 
-                    <input 
-                        type='email'
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                    />
+                    <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">Email</label>
+                    <div className='mt-1.5 mb-2.5'>
+                        <input 
+                            id='email'
+                            name='email'
+                            type='email'
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#e4627d] sm:text-sm/6"
+                        />
+                    </div>
                 </div>
                 <div>
-                    Password:
-                    <input
-                        type='password'
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                    />
+                    <label htmlFor="password" className="block text-sm/6 font-semibold text-gray-900">Password</label>
+                    <div className='mt-1.5 mb-2.5'>
+                        <input 
+                            id='password'
+                            name='password'
+                            type='password'
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#e4627d] sm:text-sm/6"
+                        />
+                    </div>
                 </div>
                 <div>
-                    Name:
-                    <input
-                        type='text'
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                    />
+                    <label htmlFor="confirmPassword" className="block text-sm/6 font-semibold text-gray-900">Confirm Password</label>
+                    <div className='mt-1.5 mb-2.5'>
+                        <input 
+                            id='confirmPassword'
+                            name='confirmPassword'
+                            type='password'
+                            value={confPassword}
+                            onChange={e => setConfPassword(e.target.value)}
+                            className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#e4627d] sm:text-sm/6"
+                        />
+                    </div>
                 </div>
-                <button
-                    className="px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300"
-                    onClick={register}
-                > 
-                    Register
-                </button>
+                <div>
+                    <label htmlFor="name" className="block text-sm/6 font-semibold text-gray-900">Name</label>
+                    <div className='mt-1.5 mb-2.5'>
+                        <input 
+                            id='name'
+                            name='name'
+                            type='text'
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                            className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#e4627d] sm:text-sm/6"
+                        />
+                    </div>
+                </div>
+                <div className='mt-10'>
+                    <button
+                        type='submit'
+                        className="block w-full rounded-md bg-[#e4627d] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-[#eb7b91] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        onClick={register}
+                    > 
+                        Register
+                    </button>
+                </div>
+                <div className='flex flex-start mt-5'>
+                    <span className="text-sm/6 text-gray-600">Already have an account?  <Link className="font-semibold text-[#eb7b91]" to="/login">Sign in here</Link></span>
+                   
+                </div>
             </form>
+            
+            
         </>
     )
 }

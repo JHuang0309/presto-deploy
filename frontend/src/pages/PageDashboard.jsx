e0dfd83c49856b065ca14b579799c24ab4f97f24
Alyssa Xia <z5420334@ad.unsw.edu.au>
2024-11-02 22:10:54 +1100

    importing packages

--- frontend/src/pages/PageDashboard.jsx
+++ frontend/src/pages/PageDashboard.jsx
@@ -1,8 +1,13 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function PageDashboard() {
     return (
         <>
             <div className="ml-4">
                 <h3 className="text-balance text-3xl font-semibold">Start a new presentation</h3>
+                {/* <div class="plus-sign"></div> */}
+                <button class="bg-gray-400 text-white p-4 rounded-md">+</button>
             </div>
             <hr className="ml-4"/>
             <div className="ml-4">

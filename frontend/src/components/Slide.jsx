0e68c16e825b47848652a87b8a13ddb073a77451
Jayden Huang <jaydenhuang@student.unsw.edu.au>
2024-11-04 22:48:00 +1100

    Added slight changes to slide spacing

--- frontend/src/components/Slide.jsx
+++ frontend/src/components/Slide.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const Slide = () => {
     return (
         <>
-            <div className='bg-white aspect-[16/9] w-full max-w-full min-w-[700px]'>
+            <div className='bg-white aspect-[16/9] max-w-full min-w-[300px] max-h-[800px] m-10'>
 
             </div>
         </>

b00959383eb839bbaea5e7ddfb9ec05bc13fdacc
Jayden Huang <jaydenhuang@student.unsw.edu.au>
2024-11-11 21:17:27 +1100

    Added tests for modal textbox

--- frontend/src/__test__/Modal.test.jsx
+++ frontend/src/__test__/Modal.test.jsx
@@ -7,7 +7,7 @@ const mockAddTextbox = vi.fn();
 const mockOnClose = vi.fn();
 const mockAddImage = vi.fn();
 
-describe('Modal component with different input types', () => {
+describe('Modal component with text input type', () => {
     beforeEach(() => {
         vi.clearAllMocks();
     });
@@ -55,49 +55,4 @@ describe('Modal component with different input types', () => {
         });
         
     });
-
-    // it('renders the image-type modal and handles input correctly', async () => {
-    //     render(
-    //         <BrowserRouter>
-    //             <Modal
-    //                 type="image"
-    //                 isOpen={true}
-    //                 onClose={mockOnClose}
-    //                 addImage={mockAddImage}
-    //             />
-    //         </BrowserRouter>
-    //     );
-
-    //     // check all input fields are rendered
-    //     expect(screen.getByLabelText(/Width \(px\)/i)).toBeInTheDocument();
-    //     expect(screen.getByLabelText(/Height \(px\)/i)).toBeInTheDocument();
-    //     expect(screen.getByLabelText(/Enter Image/i)).toBeInTheDocument();
-    //     expect(screen.getByLabelText(/Description/i)).toBeInTheDocument();
-
-    //     // simulate user input
-    //     fireEvent.change(screen.getByLabelText(/Width \(px\)/i), { target: { value: '200' } });
-    //     fireEvent.change(screen.getByLabelText(/Height \(px\)/i), { target: { value: '300' } });
-    //     fireEvent.change(screen.getByLabelText(/Enter Image/i), { target: { value: 'http://example.com/image.jpg' } });
-    //     fireEvent.change(screen.getByLabelText(/Description/i), { target: { value: 'An example image' } });
-
-    //     // const fileInput = screen.getByLabelText(/Enter Image/i).nextSibling;
-    //     // const file = new File(['sample'], 'image.jpg', { type: 'image/jpeg' });
-    //     // fireEvent.change(fileInput, { target: { files: [file] } });
-    //     // // Check that the URL input is cleared when a file is selected
-    //     // await waitFor(() => {
-    //     //     expect(screen.getByLabelText(/Enter Image/i).value).toBe(''); // check URL input is cleared
-    //     // });
-
-
-    //     fireEvent.click(screen.getByRole('button', { name: /Add/i }));
-    //     await waitFor(() => {
-    //         expect(mockAddImage).toHaveBeenCalledWith(expect.objectContaining({
-    //             width: '200',
-    //             height: '300',
-    //             image: expect.any(String),  // expect base64 string from file conversion
-    //             description: 'An example image',
-    //         }));
-    //     });
-        
-    // });
 });    

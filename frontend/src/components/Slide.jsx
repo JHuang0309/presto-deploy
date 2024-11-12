90788b5b35d3d77d4a9ad93bd01991ad6d6ae3f5
Jayden Huang <jaydenhuang@student.unsw.edu.au>
2024-11-12 21:55:06 +1100

    Committing linting fixes

--- frontend/src/components/Slide.jsx
+++ frontend/src/components/Slide.jsx
@@ -1,28 +1,26 @@
-import { useState } from 'react';
-
 const Slide = ({ elements, format }) => {
-    // console.log(elements)
-    // console.log(format)
-    if (elements == null || format == null) {
-        return (
-            <>
-                <div className='bg-white aspect-[16/9] max-w-full min-w-[300px] max-h-[800px] m-10'>
-                </div>
-            </>
-        );
-    }
+  // console.log(elements)
+  // console.log(format)
+  if (elements == null || format == null) {
     return (
-        <>
-            <div className='bg-white aspect-[16/9] max-w-full min-w-[300px] max-h-[800px] m-10'>
-                {elements.map((Element, index) => (
-                    <div key={index}>
-                        {Element}
-                    </div>
-                ))}
-            </div>
-        </>
-        
+      <>
+        <div className='bg-white aspect-[16/9] max-w-full min-w-[300px] max-h-[800px] m-10'>
+        </div>
+      </>
     );
+  }
+  return (
+    <>
+      <div className='bg-white aspect-[16/9] max-w-full min-w-[300px] max-h-[800px] m-10'>
+        {elements.map((Element, index) => (
+          <div key={index}>
+            {Element}
+          </div>
+        ))}
+      </div>
+    </>
+    
+  );
 };
 
 export default Slide;

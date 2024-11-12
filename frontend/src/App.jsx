afe955a1d4caffda15eb785d3223ae746ae0ca9e
Jayden Huang <jaydenhuang@student.unsw.edu.au>
2024-11-12 21:50:32 +1100

    Committing linting fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,30 +27,30 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      <div className='mx-auto flex justify-end p-6 lg:px-8'>
-        { token ? (
-          <>
-            <Link
-              to="/dashboard"
-              className="rounded-md bg-[#e4627d] px-3.5 py-1.5 mx-1 text-center text-sm/6 font-semibold text-white shadow-sm hover:bg-[#eb7b91] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-300"
-            >Dashboard</Link>
-            <Logout 
-              token={token}
-              setTokenFn={setToken} />
-          </>
-        ) : (
-          <>
-            <Link
-              to="/login"
-              className="rounded-md bg-white px-3.5 py-1.5 mx-1 text-center text-sm/6 font-semibold text-black shadow-sm hover:text-[#eb7b91] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-300" 
-            >Login</Link>
-            <Link 
-              to="/register"
-              className="rounded-md bg-[#e4627d] px-3.5 py-1.5 mx-1 text-center text-sm/6 font-semibold text-white shadow-sm hover:bg-[#eb7b91] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-300"
-            >Sign up</Link>
-          </>
-        )}  
-      </div>
+        <div className='mx-auto flex justify-end p-6 lg:px-8'>
+          { token ? (
+            <>
+              <Link
+                to="/dashboard"
+                className="rounded-md bg-[#e4627d] px-3.5 py-1.5 mx-1 text-center text-sm/6 font-semibold text-white shadow-sm hover:bg-[#eb7b91] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-300"
+              >Dashboard</Link>
+              <Logout 
+                token={token}
+                setTokenFn={setToken} />
+            </>
+          ) : (
+            <>
+              <Link
+                to="/login"
+                className="rounded-md bg-white px-3.5 py-1.5 mx-1 text-center text-sm/6 font-semibold text-black shadow-sm hover:text-[#eb7b91] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-300" 
+              >Login</Link>
+              <Link 
+                to="/register"
+                className="rounded-md bg-[#e4627d] px-3.5 py-1.5 mx-1 text-center text-sm/6 font-semibold text-white shadow-sm hover:bg-[#eb7b91] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition duration-300"
+              >Sign up</Link>
+            </>
+          )}  
+        </div>
         <Routes>
           <Route path="/" element={<Navigate to="/register" />} />
           <Route path="/register" element={<PageRegister setTokenFn={setToken}/>} />

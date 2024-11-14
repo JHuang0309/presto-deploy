1ab27dc0151f47bf672e877616e096727a161513
Alyssa Xia <z5420334@ad.unsw.edu.au>
2024-11-14 12:22:14 +1100

    fixing issues with dynamic styling of colour

--- frontend/src/components/Textbox.jsx
+++ frontend/src/components/Textbox.jsx
@@ -1,7 +1,7 @@
 const Textbox = ({ width, height, text, size, colour }) => {
   return (
-    <div className={`w-${width} h-${height} text-[${colour}] text-${size}xl border border-gray-100 m-5`}>
-      {width}, {height}, {text}, {size}, {colour}
+    <div className={`w-${width} h-${height} text-${size}xl border border-gray-100 m-5`}>
+      <p style={{color:`${colour}`}}>{width}, {height}, {text}, {size}, {colour}</p>
     </div>
   );
 };

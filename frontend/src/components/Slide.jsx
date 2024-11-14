73b16e4d03e22fa8b5adbd6ce256c8c66b158d73
Alyssa Xia <z5420334@ad.unsw.edu.au>
2024-11-14 11:48:28 +1100

    Textbox display

--- frontend/src/components/Slide.jsx
+++ frontend/src/components/Slide.jsx
@@ -1,6 +1,11 @@
+import Textbox from '../components/Textbox';
+
 const Slide = ({ elements, format }) => {
   // console.log(elements);
   // console.log(format);
+  
+  
+
   if (elements == null || format == null) {
     return (
       <>
@@ -9,13 +14,21 @@ const Slide = ({ elements, format }) => {
       </>
     );
   }
+
+  const Textboxes = elements.filter(element => element.props.text);
+
   return (
     <>
       <div className='bg-white aspect-[16/9] max-w-full min-w-[250px] max-h-[800px] m-10'>
+      {/* {JSON.stringify(Textboxes)} */}
+      {Textboxes.map((textbox, index) => (
+          <Textbox key={index} {...textbox.props} />
+        ))}
       </div>
     </>
     
   );
+  
 };
 
 export default Slide;

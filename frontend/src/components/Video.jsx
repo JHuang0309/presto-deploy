99e75086bb0bcc9ccb41b1153b00e12ba224296b
Alyssa Xia <z5420334@ad.unsw.edu.au>
2024-11-14 17:06:21 +1100

    Video display added

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -1,9 +1,21 @@
+import { Rnd } from 'react-rnd';
+
 const Video = ({ width, height, url, autoplay }) => {
+  let autoplayToggle = 1;
+  if (autoplay === undefined) {
+    autoplayToggle = 0;
+  }
   return (
-    <div>
-      {width}, {height}, {url}, {autoplay ? 'true' : 'false'}
+    <Rnd>
+      <div className={`w-${width} h-${height}`}>
+        <iframe width={`${width}`} height={`${height}`} src={`${url}?${autoplayToggle}`}>
+        </iframe>
+      
     </div>
+    </Rnd>
+    
   );
 };
 
 export default Video;
+

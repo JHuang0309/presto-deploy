771de46786075388012cd5fb2be120878565940f
Alyssa Xia <z5420334@ad.unsw.edu.au>
2024-11-15 18:48:35 +1100

    fixing saving position of image

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,10 +1,33 @@
 import { useState, useRef } from 'react';
 import { Rnd } from 'react-rnd';
 
-const Image = ({ width, height, image, description }) => {
+// const Image = ({ width, height, image, description }) => {
+
+//   return (
+//     <Rnd>
+//       <img className={`w-${width} h-${height}`} src={`${image}`} alt={`${description}`}/>
+//     </Rnd>
+//   );
+// };
+
+
+// export default Image;
+
+
+const Image = ({ width, height, image, description, position = { x: 0, y: 0 }, updatePosition }) => {
+  const ref = useRef(null);
+
+  const handleDragStop = (event, data) => {
+    const newPosition = { x: data.x, y: data.y }
+    updatePosition(newPosition);
+  };
 
   return (
-    <Rnd>
+    <Rnd
+      ref={ref}
+      position={position}
+      onDragStop={handleDragStop}
+    >
       <img className={`w-${width} h-${height}`} src={`${image}`} alt={`${description}`}/>
     </Rnd>
   );

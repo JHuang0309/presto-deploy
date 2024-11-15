379e08a192dde7cdfd4bc3b0a90ba53c1276073d
Alyssa Xia <z5420334@ad.unsw.edu.au>
2024-11-15 19:23:03 +1100

    linting fix

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,20 +1,9 @@
-import { useRef } from 'react';
 import { Rnd } from 'react-rnd';
 
-const Image = ({ width, height, image, description, position = { x: 0, y: 0 }, updatePosition }) => {
-  // const ref = useRef(null);
-
-  // const handleDragStop = (event, data) => {
-  //   const newPosition = { x: data.x, y: data.y }
-  //   updatePosition(newPosition);
-  // };
+const Image = ({ width, height, image, description}) => {
 
   return (
-    <Rnd
-      // ref={ref}
-      // position={position}
-      // onDragStop={handleDragStop}
-    >
+    <Rnd>
       <img className={`w-${width} h-${height}`} src={`${image}`} alt={`${description}`}/>
     </Rnd>
   );
